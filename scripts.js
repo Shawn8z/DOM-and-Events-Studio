e5a898c9ca3adb3f05fdd74b2047cc2bbca126ca
Shawn <lcorel86@gmail.com>
2022-02-28 02:21:07 -0600

    studio finish

--- scripts.js
+++ scripts.js
@@ -9,10 +9,75 @@ window.addEventListener("load", ()=>{
     land.addEventListener("click", landing);
     abort.addEventListener("click", missionAbort);
 
-
-
-
+    let rocket = document.querySelector("img");
+    let up = document.querySelector("#up");
+    let down = document.querySelector("#down");
+    let left = document.querySelector("#left");
+    let right = document.querySelector("#right");
     
+    up.addEventListener("click", moveUp);
+    down.addEventListener("click", moveDown);
+    left.addEventListener("click", moveLeft);
+    right.addEventListener("click", moveRight);
+
+
+
+    function moveLeft() {
+        let oldPos = rocket.style.left;
+        let newPos = (parseInt(oldPos) + 10);
+
+        if (newPos > 150) {
+            rocket.style.left = oldPos;
+        } else {
+            rocket.style.left = newPos  + "px";
+        } 
+    }
+
+    function moveRight() {
+        let oldPos = rocket.style.left;
+        let newPos = (parseInt(oldPos) - 10);
+
+        if (newPos < -150) {
+            rocket.style.left = oldPos;
+        } else {
+            rocket.style.left = newPos  + "px";
+        } 
+    }
+
+    function moveUp() {
+        let oldPos = rocket.offsetTop;;
+        let newPos =  (oldPos - 10);
+
+        if (newPos < 0) {
+            rocket.style.top = oldPos;
+        } else {
+            rocket.style.top = newPos  + "px";
+            let currentHeight = document.querySelector("#spaceShuttleHeight").innerHTML
+            document.querySelector("#spaceShuttleHeight").innerHTML = parseInt(currentHeight) + 10000
+        }
+
+        if (newPos < 250) {
+            document.querySelector("#flightStatus").innerHTML = "Shuttle in flight.";
+            document.querySelector("#shuttleBackground").style.backgroundColor = "blue";
+        }
+    }
+
+    function moveDown() {
+        let oldPos = rocket.offsetTop;
+        let newPos =  (oldPos + 10);
+        if (newPos === 250) {
+            document.querySelector("#flightStatus").innerHTML = "The shuttle has landed.";
+            document.querySelector("#shuttleBackground").style.backgroundColor = "green";
+        } 
+
+        if (newPos > 250) {
+            rocket.style.top = oldPos;
+        } else {
+            rocket.style.top = newPos  + "px";
+            let currentHeight = document.querySelector("#spaceShuttleHeight").innerHTML
+            document.querySelector("#spaceShuttleHeight").innerHTML = parseInt(currentHeight) - 10000;
+        }
+    }
 
 
 
@@ -25,6 +90,8 @@ window.addEventListener("load", ()=>{
 
             let currentHeight = document.querySelector("#spaceShuttleHeight").innerHTML
             document.querySelector("#spaceShuttleHeight").innerHTML = parseInt(currentHeight) + 10000;
+
+            rocket.style.top = 250 - 10 + "px";
         }
     }
 
@@ -33,6 +100,9 @@ window.addEventListener("load", ()=>{
         document.querySelector("#flightStatus").innerHTML = "The shuttle has landed.";
         document.querySelector("#shuttleBackground").style.backgroundColor = "green";
         document.querySelector("#spaceShuttleHeight").innerHTML = 0;
+
+        rocket.style.top = 250 + "px";
+        rocket.style.left = 0 + "px";
     }
 
     function missionAbort() {
@@ -41,8 +111,9 @@ window.addEventListener("load", ()=>{
             document.querySelector("#flightStatus").innerHTML = "Mission aborted.";
             document.querySelector("#shuttleBackground").style.backgroundColor = "green";
             document.querySelector("#spaceShuttleHeight").innerHTML = 0;
+
+            rocket.style.top = 250 + "px";
+            rocket.style.left = 0 + "px";
         }
     }
-
-    
 })
